26da1e25b95eda72406392c0bad91e1290adc76b
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-09-21 14:49:20 +0200

    create services component

---
+++ src/components/Services/Services.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div className='services'>
+            <div className='dark-overlay'>
+                <ul className='main-list'>
+                    <li className='main-list-item'>  
+                        <div className='package package-yellow'>
+                            <p>Pro Package</p>
+                            <ul className='secondary-list'>
+                                <li>provident omnis voluptas eligendi</li>
+                                <li>Fugiat qui explicabo excepturi</li>
+                                <li>Lorem ipsum dolor sit amet</li>
+                                <li>adipisicing elit. Quos, dicta nobis</li>
+                                <li>facilis accusantium sed eum</li>
+                                <li>ducimus reiciendis architecto</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className='main-list-item'> 
+                        <div className='package package-pink'>
+                            <p>Premium Package</p>
+                            <ul className='secondary-list'>
+                                <li>voluptatum dolores aliquid aliquam</li>
+                                <li>Lorem ipsum dolor sit amet</li>
+                                <li>adipisicing elit. Quos, dicta nobis</li>
+                                <li>facilis accusantium sed eum</li>
+                                <li>ducimus reiciendis architecto</li>
+                                <li>provident omnis voluptas eligendi</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Services;
