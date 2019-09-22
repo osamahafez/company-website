2a3df95f7b108e9cf5c1a22c5ca0283f6c268354
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-09-22 15:20:43 +0200

    add css files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Introduction from './components/Introduction/Introduction';
 import Information from './components/Information/Information';
+import Services from './components/Services/Services';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/layout/Footer/Footer';
 
 const App = () => {
   return (
     <div>
         <Introduction />
         <Information />
+        <Services />
+        <Testimonials />
+        <Footer />
     </div>
   );
 }
